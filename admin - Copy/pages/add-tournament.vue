2711d71c6085cfa3bf8a1c25960b85b47096fe2e
pronaybormon <pronaykumarbormon@gmail.com>
2025-07-27 22:29:47 +0600

    add new admim

--- admin - Copy/pages/add-tournament.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Ads Page</h1>
-  </div>
-</template>
-
-<script setup lang="ts">
-// logic here
-</script>
--- admin/plugins/jquery.client.ts
+++
@@ -1,6 +0,0 @@
-import jQuery from 'jquery';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  // console.log('jQuery plugin loaded')
-  nuxtApp.provide('jquery', jQuery)
-})
